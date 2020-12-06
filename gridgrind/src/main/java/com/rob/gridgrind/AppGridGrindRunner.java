6abd7692faac7723bb0d7e8f757aa606852c6575
Chris Ludka <binaryworkspace@gmail.com>
2020-12-06 14:04:07 -0500

    Initial push.

---
+++ gridgrind/src/main/java/com/rob/gridgrind/AppGridGrindRunner.java
@@ -0,0 +1,13 @@
+package com.rob.gridgrind;
+
+/**
+ * App Runner for Grid Grind
+ * 
+ * @author Ryan Baker
+ *
+ */
+public class AppGridGrindRunner {
+	public static void main(String[] args) {
+		System.out.println("Placeholder");
+	}
+}
