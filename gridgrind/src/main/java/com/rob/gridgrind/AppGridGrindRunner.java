b47199fb22785cc0a538df4cf495144ebf7dfed5
Jacob <Jacob@Ryan.fios-router.home>
2021-01-31 14:44:59 -0500

    Added getGrid function contract.

--- gridgrind/src/main/java/com/rob/gridgrind/AppGridGrindRunner.java
+++ gridgrind/src/main/java/com/rob/gridgrind/AppGridGrindRunner.java
@@ -37,6 +37,14 @@ public class AppGridGrindRunner {
 
 	}
 
+	/**
+	 * 
+	 * @param rowCount The row size (y-axis).
+	 * @param colCount The column size (x-axis).
+	 * @throw RuntimeException: Will not except nonpositive integers for either parameters.
+	 * @return StringBuilder representation of grid
+	 * 
+	 */
 	public static String getGrid(int rowCount, int colCount) {
 
 		// Validate row count
